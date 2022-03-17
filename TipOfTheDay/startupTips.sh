bb8ba2f0ee120de7e30390bed8553d6db2d477df
vqanar <anmol.arora@tuni.fi>
2022-03-17 14:38:00 +0200

    tips with prompt for more tips

--- TipOfTheDay/startupTips.sh
+++ TipOfTheDay/startupTips.sh
@@ -5,26 +5,49 @@
 # DATE=$(date -I)
 
 # Read current tip
-NUM=$(<.curtip )
+#cat 1 > .curtip
 
+
+HOMEDIR=$HOME/scripts
+
+_showTip()
+{
+	NUM=$(<$HOMEDIR/.curtip )
+#NUM=1
 # Counting number of files in Tips folder and storing to variable
 
-LAST=$(ls -rv Tips/ | head -1 | cut -d . -f1)
+	LAST=$(ls -rv $HOMEDIR/Tips/ | head -1 | cut -d . -f1)
 
 # Another way ls *.txt | wc -l
 
-
 # Reads file 1.txt
-cat Tips/$NUM.txt
+	if [ -f "$HOMEDIR/Tips/$NUM.txt" ]; then
+#	echo "I reached here"
+		cat $HOMEDIR/Tips/$NUM.txt
+	else
+		echo "Tip doesnt exist"
+	fi
 
 # Adds one to NUM variable
-NUM=`expr $NUM + 1`
+	NUM=`expr $NUM + 1`
 
-echo $NUM> .curtip
-if [ $NUM -gt $LAST ]
-	then NUM=1 echo "1" > .curtip
-fi
+	echo $NUM>$HOMEDIR/.curtip
+	if [ $NUM -gt $LAST ];
+		then NUM=1 echo "1" > $HOMEDIR/.curtip
+	fi
+}
 
 
+_main()
+{
+	_showTip
+	echo "Show another Tip? Y/N "
+	read input
+	if [[ $input == 'Y' || $input == 'y' ]]; 
+	then _main
+	fi	
+}
+
+_main
 ######################### EOF
 
